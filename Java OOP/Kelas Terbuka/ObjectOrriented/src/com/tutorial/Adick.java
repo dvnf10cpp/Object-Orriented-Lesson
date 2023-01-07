765982ccd5b5fc86a86702ad6cfadc6fb8e2664b
Devan F <116249842+devanfer02@users.noreply.github.com>
2023-01-07 18:06:39 +0700

    Add files via upload

---
+++ Java OOP/Kelas Terbuka/ObjectOrriented/src/com/tutorial/Adick.java
@@ -0,0 +1,17 @@
+
+
+public class Adick {
+    private String name;
+
+    Adick(String name){
+        this.name = name;
+    }
+
+    void halodek(){
+        System.out.printf("Nama Adick : %s\n",this.name);
+    }
+
+    String getWant(){
+        return this.name + " want to blow mondstadt";
+    }
+}
