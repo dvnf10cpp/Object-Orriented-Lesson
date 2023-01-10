89eae29cf3725b4ea952b42688e6407982e69bf9
Devan F <116249842+devanfer02@users.noreply.github.com>
2023-01-11 00:46:27 +0700

    Update MainSuper.java

--- Java OOP/Kelas Terbuka/ObjectOrriented/src/com/superapplication/MainSuper.java
+++ Java OOP/Kelas Terbuka/ObjectOrriented/src/com/superapplication/MainSuper.java
@@ -2,6 +2,18 @@ package com.superapplication;
 
 public class MainSuper {
     public static void main(String[] args) {
+        demoSuperConstructor();
+    }
+
+    static void demoSuperConstructor(){
+        Employee employee1 = new Employee("Axel",19);
+        Designer designer1 = new Designer("John",22,"Lead Division");
+
+        employee1.work();
+        designer1.work();
+    }
+
+    static void demoSuper(){
         Student student1 = new Student();
         Programmer programmer1 = new Programmer();
 
