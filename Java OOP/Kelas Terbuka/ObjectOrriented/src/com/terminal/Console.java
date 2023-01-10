d95762d53fd5968fe9d6efa01524c6f695ddc807
Devan F <116249842+devanfer02@users.noreply.github.com>
2023-01-10 17:47:50 +0700

    Update Console.java

--- Java OOP/Kelas Terbuka/ObjectOrriented/src/com/terminal/Console.java
+++ Java OOP/Kelas Terbuka/ObjectOrriented/src/com/terminal/Console.java
@@ -1,7 +1,39 @@
 package com.terminal;
 
 public class Console {
+    //Penerapan overloading method
+    public static void logf(int width, char argument){
+        System.out.printf("%"+width +"c",argument);
+    }
+
+    public static void logf(int width, double argument){
+        System.out.printf("%"+width +"f",argument);
+    }
+
+    public static void logf(int width, int argument){
+        System.out.printf("%"+width +"d",argument);
+    }
+    public static void logf(int width, String argument){
+        System.out.printf("%"+width +"s",argument);
+    }
+
     public static void log(String message){
         System.out.println(message);
+    }
+
+    public static void log(int message){
+        System.out.println(message);
+    }
+    
+    public static void log(double message){
+        System.out.println(message);
+    }
+
+    public static void log(char message){
+        System.out.println(message);
+    }
+
+    public static void log(){
+        System.out.println("");
     }
 }
